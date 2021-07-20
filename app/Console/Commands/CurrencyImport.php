1dc3eb46059f8dd1a0d4c72f3976c5ecd3adbf44
Uldis <uldisole@Ulldiss-MacBook-Pro.local>
2021-07-20 23:15:22 +0300

    Added migrations for currencies and rates, added respective Models and created initial currency rate import

---
+++ app/Console/Commands/CurrencyImport.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Currency;
+use App\Models\CurrencyRate;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+
+class CurrencyImport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'currency:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import currency rates';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $xml_string = file_get_contents('https://www.bank.lv/vk/ecb_rss.xml');
+        $xml = simplexml_load_string($xml_string);
+        $channel = (array)$xml->channel;
+
+        foreach ((array)$channel['item'] as $channelItem) {
+            $item = (array)$channelItem;
+            $currencyString = (string)$item['description'];
+
+            $data = collect(array_chunk(explode(' ', $currencyString), 2));
+            $filter = $data->filter(function($item) {
+                return !empty($item[0]);
+            });
+            $currencyList = $filter->map(function($item) {
+                return [$item[0] => $item[1]];
+            })->collapse();
+            $dateString = (string)$item['pubDate'];
+            $ratePublishDate = Carbon::parse($dateString)->format('Y-m-d H:i:s');
+
+            foreach ($currencyList as $currency => $rate) {
+                $currencyModel = Currency::firstOrCreate([
+                    'name' => $currency,
+                ]);
+                $currencyRateModel = CurrencyRate::firstOrNew([
+                    'currency_id' => $currencyModel->id,
+                    'datetime' => $ratePublishDate,
+                ]);
+                $currencyRateModel->currency_id = $currencyModel->id;
+                $currencyRateModel->rate = $rate;
+                $currencyRateModel->save();
+            }
+        }
+    }
+}
